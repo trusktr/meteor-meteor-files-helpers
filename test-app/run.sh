3ad1bfd303afa501c25d4af003b67548f336eeb3
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-05 01:51:48 +0200

    Fixes test environment variables for Mac OS X

--- test-app/run.sh
+++ test-app/run.sh
@@ -4,13 +4,14 @@ export JASMINE_CLIENT_UNIT=0
 export JASMINE_CLIENT_INTEGRATION=0
 export JASMINE_SERVER_UNIT=0
 export JASMINE_SERVER_INTEGRATION=1
+export JASMINE_SERVER_MIRROR_APP_PATH=/Users/jonas/tmp/meteor-test-run
 
 # You need to set this environment variables to the correct values
 # before you run the tests.
-export EXPECTED_APP_PATH=/Users/jonas/Projects/meteor-files-helpers/test-app
-export EXPECTED_METEOR_TOOL_PATH=/Users/jonas/.meteor/packages/meteor-tool/1.1.2/mt-os.osx.x86_64
+export EXPECTED_APP_PATH=/Users/jonas/tmp/meteor-test-run
+export EXPECTED_METEOR_TOOL_PATH=/Users/jonas/.meteor/packages/velocity_meteor-tool/1.1.1_1/mt-os.osx.x86_64
 export EXPECTED_METEOR_INSTALLATION_PATH=/Users/jonas/.meteor
-export EXPECTED_LOCAL_PACKAGE_NODE_MODULE_PATH=
-export EXPECTED_INSTALLED_PACKAGE_NODE_MODULE_PATH=
+export EXPECTED_LOCAL_PACKAGE_NODE_MODULE_PATH=/Users/jonas/tmp/meteor-test-run/.meteor/local/isopacks/sanjo_node-module-test/npm/node_modules/fs-extra
+export EXPECTED_INSTALLED_PACKAGE_NODE_MODULE_PATH=/Users/jonas/tmp/meteor-test-run/.meteor/local/build/programs/server/npm/ddp/node_modules/sockjs
 
 meteor --test --port 5000
